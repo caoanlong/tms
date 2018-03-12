ecb34f1a5cfb18d38afab1278ec2a55ec822bbc6
Caoanlong <caoanlong@gmail.com>
2018-03-12 16:03:28 +0800

    添加菜单

--- src/assets/data/menus.js
+++ src/assets/data/menus.js
@@ -4,23 +4,40 @@ const menus = [
 		Href: '/home',
 		Name: '首页',
 		IsShow: '1',
-		Icon: 'home-icon',
-		children: []
+		Icon: 'home-icon'
 	},
 	{
-		Target: 'home',
-		Href: '/home',
-		Name: '首页',
+		Target: 'carrierbill',
+		Href: '/carrierbill',
+		Name: '承运单',
 		IsShow: '1',
 		Icon: 'home-icon',
+		redirect: '/carrierbills',
 		children: [
 			{
-				Target: 'home',
-				Href: '/home',
-				Name: '首页',
+				Target: 'carrierbills',
+				Href: '/carrierbills',
+				Name: '承运单列表',
 				IsShow: '1',
-				Icon: 'home-icon',
-				children: []
+				Icon: 'home-icon'
+			},
+			{
+				Target: 'addcarrierbill',
+				Href: '/addcarrierbill',
+				Name: '添加承运单',
+				IsShow: '0'
+			},
+			{
+				Target: 'editcarrierbill',
+				Href: '/editcarrierbill',
+				Name: '编辑承运单',
+				IsShow: '0'
+			},
+			{
+				Target: 'viewcarrierbill',
+				Href: '/viewcarrierbill',
+				Name: '查看承运单',
+				IsShow: '0'
 			}
 		]
 	}
