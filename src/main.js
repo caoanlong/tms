402b86752ffb16c7b6b67741c92e44854918a4a8
Caoanlong <caoanlong@gmail.com>
2018-03-12 10:58:45 +0800

    添加依赖

--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+
+import 'normalize.css/normalize.css'
+
+import Element from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import './assets/styles/index.css'
+import './assets/styles/sidebar.css'
+
 import App from './App'
 import router from './router'
 
