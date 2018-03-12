815bac62007c5e8abb30b6dbc9899e8a86cd711a
Caoanlong <caoanlong@gmail.com>
2018-03-12 16:30:02 +0800

    gg

--- src/router/index.js
+++ src/router/index.js
@@ -6,19 +6,54 @@ import Layout from '@/components/Layout'
 
 Vue.use(Router)
 
-let routerMap = [{
-  path: '',
-  component: Layout,
-  children: [{
-    path: '/',
-    name: 'home',
-    meta: {
-      title: '首页'
-    },
-    component: () =>
-      import ('../components/Home')
-  }]
-}]
+let routerMap = [
+	{
+		path: '',
+		component: Layout,
+		children: [
+			{
+				path: '/',
+				name: 'home',
+				meta: {
+					title: '首页'
+				},
+				component: () => import ('../components/Home')
+			},
+			{
+				path: '/carrierbill',
+				name: 'carrierbill',
+				meta: {
+					title: '承运单'
+				},
+				component: () => import ('../components/Carrierbill'),
+				redirect: '/carrierbills',
+				children: [
+					{
+						path: '/carrierbills',
+						name: 'carrierbills',
+						meta: {
+							title: '承运单列表'
+						},
+					},
+					{
+						path: '/addcarrierbills',
+						name: 'addcarrierbills',
+						meta: {
+							title: '添加承运单'
+						},
+					},
+					{
+						path: '/controls',
+						name: 'controls',
+						meta: {
+							title: '调度列表'
+						},
+					}
+				]
+			},
+		]
+	}
+]
 const router = new Router({
   scrollBehavior: () => ({
     y: 0
