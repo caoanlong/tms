bae3e60e790e70cb903d3cda14ef88a8edc24aab
Caoanlong <caoanlong@gmail.com>
2018-04-09 18:30:00 +0800

    gg

--- src/components/Carrierbill/Addcarrierbill.vue
+++ src/components/Carrierbill/Addcarrierbill.vue
@@ -22,7 +22,14 @@
 				<el-col :span="8">
 					<el-form label-width="100px">
 						<el-form-item label="承运人">
-							<el-input placeholder="请输入承运人"></el-input>
+							<el-input placeholder="请输入承运人" v-model="carrierbillInfo.carrierrName"></el-input>
+						</el-form-item>
+					</el-form>
+				</el-col>
+				<el-col :span="8">
+					<el-form label-width="100px">
+						<el-form-item label="承运单号">
+							<el-input placeholder="请输入承运单号" v-model="carrierbillInfo.carrierOrderNo"></el-input>
 						</el-form-item>
 					</el-form>
 				</el-col>
@@ -46,7 +53,7 @@
 							<el-input placeholder="发货人" v-model="shipperDetail.contactName" disabled></el-input>
 						</el-form-item>
 						<el-form-item label="发货时间">
-							<el-date-picker type="datetime" style="width:100%" placeholder="选择发货时间" v-model="carrierbillInfo.shipperDate" value-format="timestamp" ></el-date-picker>
+							<el-date-picker style="width:100%" placeholder="选择发货时间" v-model="carrierbillInfo.shipperDate" value-format="timestamp" ></el-date-picker>
 						</el-form-item>
 					</el-form>
 				</el-col>
@@ -105,7 +112,7 @@
 				<el-col :span="8">
 					<el-form label-width="100px">
 						<el-form-item label="到货时间">
-							<el-date-picker type="datetime" style="width:100%" placeholder="选择到货时间" v-model="carrierbillInfo.consigneeDate" value-format="timestamp"></el-date-picker>
+							<el-date-picker style="width:100%" placeholder="选择到货时间" v-model="carrierbillInfo.consigneeDate" value-format="timestamp"></el-date-picker>
 						</el-form-item>
 					</el-form>
 				</el-col>
@@ -149,10 +156,10 @@
 				<el-col :span="24">
 					<el-form label-width="100px">
 						<el-form-item label="货物信息">
-							<div class="cargoItem" v-for="(item,index) in cargoInfo">
-								<el-select v-model="item.type" placeholder="请选择" style="width:100px">
-									<el-option label="重货" value="重货"></el-option>
-									<el-option label="轻货" value="轻货"></el-option>
+							<div class="cargoItem" v-for="(item,index) in carrierbillInfo.carrierCargoInfo">
+								<el-select v-model="item.weightType" placeholder="请选择" style="width:100px">
+									<el-option label="重货" value="Heavy"></el-option>
+									<el-option label="轻货" value="Light"></el-option>
 								</el-select>
 								<el-input placeholder="货物名称" style="width:150px" v-model="item.cargoName"></el-input>
 								<el-input placeholder="货物规格" style="width:150px" v-model="item.cargoType"></el-input>
@@ -160,7 +167,7 @@
 								<el-input placeholder="货物体积" style="width:150px" v-model="item.cargoVolume"><span slot="suffix">方</span></el-input>
 								<el-input placeholder="货物数量" style="width:150px" v-model="item.cargoNum"><span slot="suffix">件</span></el-input>
 								
-								<el-button type="text" icon="el-icon-delete" style="color:#F56C6C" @click="removeItem(index)" v-show="cargoInfo.length>1">删除</el-button>
+								<el-button type="text" icon="el-icon-delete" style="color:#F56C6C" @click="removeItem(index)" v-show="carrierbillInfo.carrierCargoInfo.length>1">删除</el-button>
 							</div>
 						</el-form-item>
 					</el-form>
@@ -176,11 +183,11 @@
 				<el-col :span="24">
 					<el-form label-width="100px">
 						<el-form-item label="承运单应收款">
-							<el-radio-group v-model="carrierbillInfo.porRequireMethod">
-								<el-radio label="Y">按吨公里自动生成</el-radio>
-								<el-radio label="N">手动输入</el-radio>
+							<el-radio-group v-model="carrierbillInfo.receiptMethod">
+								<el-radio label="TKM">按吨公里自动生成</el-radio>
+								<el-radio label="Manual">手动输入</el-radio>
 							</el-radio-group>
-							<div v-show="carrierbillInfo.porRequireMethod=='Y'" class="tips">从“这个单的发货地”到卸货地对外运距为“50公里”总价为90909009？</div>
+							<div v-show="carrierbillInfo.receiptMethod=='TKM'" class="tips">从“这个单的发货地”到卸货地对外运距为“50公里”总价为90909009？</div>
 							<div class="form-input">
 								<el-form-item label="现付" label-width="40px" style="width:180px;display:inline-block;margin:10px 10px 0 0">
 									<el-input placeholder="现付" v-model="carrierbillInfo.cashAmount"></el-input>
@@ -201,10 +208,10 @@
 						</el-form-item>
 						<el-form-item label="付款费用">
 							<el-radio-group v-model="carrierbillInfo.paymentMethod">
-								<el-radio label="Y">按吨公里自动生成</el-radio>
-								<el-radio label="N">手动输入</el-radio>
+								<el-radio label="TKM">按吨公里自动生成</el-radio>
+								<el-radio label="Manual">手动输入</el-radio>
 							</el-radio-group>
-							<div v-show="carrierbillInfo.paymentMethod=='Y'" class="tips">从“这单的发货地”到卸货地对内运距为“50公里”?</div>
+							<div v-show="carrierbillInfo.paymentMethod=='TKM'" class="tips">从“这单的发货地”到卸货地对内运距为“50公里”?</div>
 						</el-form-item>
 						<el-form-item label="发票">
 							<el-radio-group v-model="carrierbillInfo.Invoice">
@@ -270,7 +277,16 @@ export default {
 				customerID:''
 			},
 			carrierbillInfo: {
-				carrierCargoInfo:[],
+				carrierCargoInfo:[
+					{
+						'weightType': '',
+						'cargoName': '',
+						'cargoType': '',
+						'cargoWeight': '',
+						'cargoVolume': '',
+						'cargoNum': ''
+					}
+				],
 				carrierOrderNo:'',
 				carrierrName:'',
 				cashAmount:'',
@@ -288,9 +304,9 @@ export default {
 				consigneePhone:'',
 				consignorID:'',
 				monthlyAmount:'',
-				paymentMethod:'',
+				paymentMethod:'Manual',
 				porAmount:'',
-				receiptMethod:'',
+				receiptMethod:'Manual',
 				shipperAddressID:'',
 				shipperArea:'',
 				shipperCompanyName:'',
@@ -303,14 +319,6 @@ export default {
 				Invoice:'N',
 				porRequire: ['NotRequired'],
 			},
-			cargoInfo: [{
-				'cargoType': '',
-				'cargoName': '',
-				'cargoType': '',
-				'cargoWeight': '',
-				'cargoVolume': '',
-				'cargoNum': ''
-			}],
 			transType:'',
 			transTypeOption:[
 				{
@@ -414,6 +422,7 @@ export default {
 			console.log('发货人详情')
 			console.log(arr[0])
 			this.shipperDetail.contactName = arr[0].contactName
+			this.shipperDetail.contactPhone = arr[0].contactPhone
 			this.shipperDetail.companyArea = arr[0].companyArea
 			this.shipperDetail.detailAddress = arr[0].detailAddress
 			this.shipperDetail.companyID = arr[0].companyID
@@ -421,13 +430,14 @@ export default {
 			let areaID = String(arr[0].companyAreaID)
 			this.selectedArea = [(areaID.substr(0, 2) + '0000'), (areaID.substr(0, 4) + '00'), areaID]
 		},
-			getConsigneeDetail(e){
+		getConsigneeDetail(e){
 			let arr = this.Recdeliverycomp.filter(item => {
 				return item.companyName == e
 			})
 			console.log('收货人详情')
 			console.log(arr[0])
 			this.ConsigneeDetail.contactName = arr[0].contactName
+			this.ConsigneeDetail.contactPhone = arr[0].contactPhone
 			this.ConsigneeDetail.companyArea = arr[0].companyArea
 			this.ConsigneeDetail.detailAddress = arr[0].detailAddress
 			this.ConsigneeDetail.companyID = arr[0].companyID
@@ -437,10 +447,8 @@ export default {
 		},
 		
 		save(){
-			// console.log(this.shipperDetail)
-			// return
 			let data = {
-				carrierCargoInfo:this.carrierCargoInfo,
+				carrierCargoInfo:JSON.stringify(this.carrierbillInfo.carrierCargoInfo),
 				carrierOrderNo:this.carrierbillInfo.carrierOrderNo,
 				carrierrName:this.carrierbillInfo.carrierrName,
 				cashAmount:this.carrierbillInfo.cashAmount,
@@ -448,13 +456,13 @@ export default {
 				consigneeAddressID:this.carrierbillInfo.consigneeAddressID,
 				consigneeAmount:this.carrierbillInfo.consigneeAmount,
 				consigneeArea:this.ConsigneeDetail.companyArea,
-				consigneeAreaID:this.selectedArea,
+				consigneeAreaID:this.selectedArea.join(','),
 				consigneeCompanyName:this.carrierbillInfo.consigneeCompanyName,
 				consigneeDate:this.carrierbillInfo.consigneeDate,
-				consigneeDetailAddress:this.carrierbillInfo.consigneeDetailAddress,
+				consigneeDetailAddress:this.ConsigneeDetail.detailAddress,
 				consigneeID:this.ConsigneeDetail.customerID,
-				consigneeName:this.carrierbillInfo.consigneeName,
-				consigneePhone:this.carrierbillInfo.consigneePhone,
+				consigneeName:this.ConsigneeDetail.contactName,
+				consigneePhone:this.ConsigneeDetail.contactPhone,
 				consignorID:this.Shippers.customerID,
 				monthlyAmount:this.carrierbillInfo.monthlyAmount,
 				paymentMethod:this.carrierbillInfo.paymentMethod,
@@ -468,7 +476,7 @@ export default {
 				shipperID:this.shipperDetail.customerID,
 				shipperName:this.shipperDetail.contactName,
 				shipperNo:this.carrierbillInfo.shipperNo,
-				shipperPhone:this.carrierbillInfo.shipperPhone
+				shipperPhone:this.shipperDetail.contactPhone
 			}
 			console.log(data)
 			request({
@@ -482,7 +490,7 @@ export default {
 			})
 		},
 		addItem() {
-			this.cargoInfo.push({
+			this.carrierbillInfo.carrierCargoInfo.push({
 				'cargoType': '',
 				'cargoName': '',
 				'weightType': '',
@@ -492,7 +500,7 @@ export default {
 			})
 		},
 		removeItem(index) {
-			this.cargoInfo.splice(index,1)
+			this.carrierbillInfo.carrierCargoInfo.splice(index, 1)
 		},
 		selectReceipt(e){
 			if (this.carrierbillInfo.porRequire.includes(e)) {
