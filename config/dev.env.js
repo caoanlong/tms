71686616607290bd23b976dd0044ebf2366a4a5a
Caoanlong <caoanlong@gmail.com>
2018-04-10 09:35:54 +0800

    gg

--- config/dev.env.js
+++ config/dev.env.js
@@ -5,12 +5,12 @@ const prodEnv = require('./prod.env')
 module.exports = merge(prodEnv, {
 	NODE_ENV: '"development"',
 	// 张颜电脑
-	// BASE_API: '"http://192.168.1.29:8080"',
+	// BASE_API: '"http://192.168.1.29:9999"',
 	// 飞龙电脑
-	BASE_API: '"http://192.168.1.43:8080"',
+	// BASE_API: '"http://192.168.1.43:9999"',
 	// 军良电脑
-	// BASE_API: '"http://192.168.1.99:8080"',
+	BASE_API: '"http://192.168.1.99:9999"',
 	// 农新考电脑
-	// BASE_API: '"http://192.168.1.49:8080"',
+	// BASE_API: '"http://192.168.1.49:9999"',
 	IMG_API: '"http://develop.we-service.cn/hdd/image/"',
 })
