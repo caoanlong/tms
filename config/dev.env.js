114c6487cef6e2b3573b0679cf1e3ff15bde6735
Caoanlong <caoanlong@gmail.com>
2018-04-14 09:52:22 +0800

    ggg

--- config/dev.env.js
+++ config/dev.env.js
@@ -14,5 +14,7 @@ module.exports = merge(prodEnv, {
 	BASE_API: '"http://192.168.1.60:9999"',
 	// 农新考电脑
 	// BASE_API: '"http://192.168.1.49:9999"',
+	// 测试
+	// BASE_API: '"http://develop.we-service.cn/tms-api/"',
 	IMG_API: '"http://develop.we-service.cn/hdd/image/"',
 })
