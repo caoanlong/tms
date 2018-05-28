26d71273a88b207c71f6f9a8a71ad8769b7c12b1
coolia <gj@vfu56.com>
2018-05-28 09:45:00 +0800

    cccccc

--- src/components/DispatchBill/Common/TaskItem.vue
+++ src/components/DispatchBill/Common/TaskItem.vue
@@ -1,31 +1,46 @@
 <template>
 	<div class="item">
-		<table class="lineInfo">
-			<tr>
-				<td colspan="2" class="tit"><span class="justify">任务单号</span>919239801 <span class="status fr">待装车</span></td>
-			</tr>
-			<tr>
-				<td><span class="justify">发货地</span>云南昆明官渡区浩宏物流园</td>
-				<td><span class="justify">收货地</span>广东广州朱家村云海路1123号广元自贸区</td>
-			</tr>
-			<tr>
-				<td><span class="justify">联系人</span>朱静静</td>
-				<td><span class="justify">联系人</span>郝晓晓</td>
-			</tr>
-			<tr>
-				<td><span class="justify">联系电话</span>13800138000</td>
-				<td><span class="justify">联系电话</span>13800138001</td>
-			</tr>
-			
-			<tr>
-				<td>
-					<span class="justify">发货时间</span>2018-06-13 17:51
-				</td>
-				<td>
-					<span class="justify">到货时间</span>2018-06-25 14:24
-				</td>
-			</tr>
-		</table>
+		<div class="lineInfo">
+			<div class="tit"><span>任务单号：919239801</span> <span class="status fr">待装车</span></div>
+			<div class="con">
+				<div class="from">
+					<img src="../../../assets/imgs/avatar.gif" class="headPic"/>
+					<p class="companyName">深圳飞天贸易</p>
+					<p>13424389894</p>
+					<p class="area">广东深圳</p>
+					<p class="datetime">2018-06-13 17:51（预计发货）</p>
+					<p class="datetime c2">2018-06-25 14:24（实际发货）</p>
+				</div>
+				<div class="platNo"><span>粤BT63Y6</span></div>
+				<div class="to">
+					<img src="../../../assets/imgs/avatar.gif" class="headPic"/>
+					<p class="companyName">库那天交运贸易</p>
+					<p>13424389894</p>
+					<p class="area">云南昆明</p>
+					<p class="datetime">2018-06-15 13:24（预计到货）</p>
+				</div>
+			</div>
+		</div>
+		<div class="personInfo">
+			<div class="tit">人员信息</div>
+			<div class="con">
+				<div class="driver">
+					<img src="../../../assets/imgs/avatar.gif" class="headPic"/>
+					<p class="name">吴郑平<span class="tag">司机</span></p>
+					<p>13424389894</p>
+				</div>
+				<div class="escort">
+					<img src="../../../assets/imgs/avatar.gif" class="headPic"/>
+					<p class="name">吴郑平<span class="tag">司机</span></p>
+					<p>13424389894</p>
+				</div>
+				<div class="dispatcher">
+					<img src="../../../assets/imgs/avatar.gif" class="headPic"/>
+					<p class="name">吴郑平<span class="tag">司机</span></p>
+					<p>13424389894</p>
+				</div>
+			</div>
+		</div>
 		<table class="cargoList">
 			<tr>
 				<td class="tit" colspan="4">货物清单</td>
@@ -89,10 +104,6 @@ export default {
 		border-spacing 1px
 		font-size 14px
 		margin-bottom 10px
-		&.lineInfo
-			td
-				width 50%
-				padding 6px 10px 6px 90px
 		td
 			background #fff
 			padding 6px 10px
@@ -126,4 +137,85 @@ export default {
 			height 36px
 			line-height 24px
 			color #666
+	.lineInfo
+	.personInfo
+		font-size 14px
+		.tit
+			background #e2ecf6
+			color #3582d0
+			padding 6px 10px
+			height 36px
+			line-height 24px
+		.con
+			display flex
+			border 1px solid #e2ecf6
+			border-top none
+			margin-bottom 10px
+			color #666
+			padding 10px
+			p
+				margin 0
+				line-height 30px
+				&.datetime
+					line-height 20px
+					font-size 12px
+					color #999
+				&.c2
+					color #f90
+			.from
+			.to
+			.driver
+			.dispatcher
+			.escort
+				padding-left 60px
+				position relative
+				.headPic
+					width 40px
+					height 40px
+					border-radius 20px
+					position absolute
+					left 10px
+					top 10px
+				.companyName
+					font-size 16px
+					color #333
+				.area
+					font-weight bold
+					color #000
+			div
+				flex 1
+			.driver
+			.dispatcher
+			.escort
+				height 60px
+				.name
+					font-size 16px
+					.tag
+						margin-left 20px
+						color #fff
+						font-size 12px
+						padding 2px 10px
+						border-radius 10px
+			.driver
+				.tag
+					background #f90
+			.dispatcher
+				.tag
+					background #69f
+			.escort
+				border-left 1px solid #f8f8f8
+				border-right 1px solid #f8f8f8
+				.tag
+					background #69c
+			.platNo
+				position relative
+				border-left 1px solid #f8f8f8
+				border-right 1px solid #f8f8f8
+				span
+					position absolute
+					top 50%
+					left 50%
+					transform translate(-50%,-50%)
+					font-size 18px
+				
 </style>
