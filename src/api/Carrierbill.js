0634fbac42ec0c1e2c5c4743aedaf1e69cfb72eb
Caoanlong <caoanlong@gmail.com>
2018-06-04 11:08:45 +0800

    ggg

--- src/api/Carrierbill.js
+++ src/api/Carrierbill.js
@@ -5,10 +5,46 @@ class Carrierbill extends Base {
     constructor(url, req) {
         super(url, req)
     }
+    /**
+     * 根据承运单查询调度单列表
+     * @param {carrierOrderID} params 承运单ID
+     */
     findDispacthBills(params) {
         return new Promise((resolve, reject) => {
             this.request({
                 url: this.baseUrl + '/findDispatchCargos',
+                params
+            }).then(res => {
+                resolve(res.data.data)
+            })
+        })
+    }
+    /**
+     * 根据承运单查询运输列表
+     * @param {carrierOrderID} params 承运单ID
+     */
+    findTransports(params) {
+        return new Promise((resolve, reject) => {
+            this.request({
+                // todo url
+                url: this.baseUrl + '',
+                params
+            }).then(res => {
+                resolve(res.data.data)
+            })
+        })
+    }
+    /**
+     * 根据承运单查询待调度列表
+     * @param {keyword} params 关键字（承运单号、货物名称、起始地、目的地）
+     * @param {beginDate} params 发货时间起
+     * @param {endDate} params 发货时间止
+     * @param {customerID} params 客户ID（收发货单位）
+     */
+    findPreDispatch(params) {
+        return new Promise((resolve, reject) => {
+            this.request({
+                url: this.baseUrl + '/preDispatch',
                 params
             }).then(res => {
                 resolve(res.data.data)
--- src/api/Dispatchbill.js
+++ src/api/Dispatchbill.js
@@ -5,6 +5,64 @@ class Dispatchbill extends Base {
     constructor(url, req) {
         super(url, req)
     }
+    /**
+     * 重新调度
+     * @param {dispatchOrderID} params 调度单ID
+     * @param {dispatchTaskCargoInfo} params 调度货物信息  
+     * @param {dispatchTaskInfo} params 承运单费用信息
+     * @param {sumAmount} params 总运费
+     * @param {loadWeightSum} params 总装载吨数
+     * @param {loadVolumeSum} params 总装载方数
+     * @param {loadNumSum} params 总装载数量
+     * @param {transportRecordID} params 运输档案ID
+     * @param {truckID} params 车辆ID
+     * @param {trailerID} params 挂车ID
+     * @param {driverID} params 司机ID
+     * @param {superCargoID} params 押运员ID
+     */
+    reLoad(data) {
+        this.request({
+            url: this.baseUrl + '/anew',
+            method: 'post',
+            data
+        })
+    }
+    /**
+     * 取消调度单
+     * @param {dispatchOrderID} params 调度单ID
+     */
+    cancel(data) {
+        this.request({
+            url: this.baseUrl + '/cancel',
+            method: 'post',
+            data
+        })
+    }
+    /**
+     * 关闭调度单
+     * @param {dispatchOrderID} params 调度单ID
+     */
+    cancel(data) {
+        this.request({
+            url: this.baseUrl + '/close',
+            method: 'post',
+            data
+        })
+    }
+    /**
+     * 根据承运单获取待配载列表
+     * @param {carrierOrderIDs} params 承运单ID（多个承运单ID）
+     */
+    findPreLoad(params) {
+        return new Promise((resolve, reject) => {
+            this.request({
+                url: this.baseUrl + '/preLoad',
+                params
+            }).then(res => {
+                resolve({data: res.data.data, cargos: res.data.cargos})
+            })
+        })
+    }
 }
 
 export default new Dispatchbill('/biz/dispatchOrder', request)
