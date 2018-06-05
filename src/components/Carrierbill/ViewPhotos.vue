99e3c039999ca7fd5bb614ba17860c81743befa1
coolia <gj@vfu56.com>
2018-06-05 15:36:34 +0800

    cccccc

--- src/components/Carrierbill/ViewPhotos.vue
+++ src/components/Carrierbill/ViewPhotos.vue
@@ -1,48 +1,47 @@
 <template>
-    <el-dialog title="查看照片" :show-close="false" :close-on-click-modal="false" :visible.sync="visible" width="80%">
-        <el-form label-width="100px">
-            <el-form-item label="装车照片">
-                <ImageUpload :limitNum="10" :isPreview="true"></ImageUpload>
-            </el-form-item>
-            <el-form-item label="到货照片">
-                <ImageUpload :limitNum="10" :isPreview="true"></ImageUpload>
-            </el-form-item>
-            <el-form-item label="回单照片">
-                <ImageUpload :limitNum="10" :isPreview="true"></ImageUpload>
-            </el-form-item>
-            <el-form-item label="异常上报">
-                <ImageUpload :limitNum="10" :isPreview="true"></ImageUpload>
-            </el-form-item>
-        </el-form>
-        <div slot="footer" class="dialog-footer">
-            <el-button @click="cancel">返回</el-button>
-        </div>
-    </el-dialog>
+	<el-dialog title="查看照片" :show-close="false" :close-on-click-modal="false" :visible.sync="visible" width="80%" class="ViewPhotosDialog">
+		<p>装车地：云南昆明</p>
+		<p>送货地：云南昆明</p>
+		<el-form label-width="100px">
+			<el-form-item label="装车照片">
+				<ImageUpload :limitNum="10" :isPreview="true"></ImageUpload>
+			</el-form-item>
+			<el-form-item label="到货照片">
+				<ImageUpload :limitNum="10" :isPreview="true"></ImageUpload>
+			</el-form-item>
+			<el-form-item label="回单照片">
+				<ImageUpload :limitNum="10" :isPreview="true"></ImageUpload>
+			</el-form-item>
+			<el-form-item label="异常上报">
+				<ImageUpload :limitNum="10" :isPreview="true"></ImageUpload>
+			</el-form-item>
+		</el-form>
+		<div slot="footer" class="dialog-footer">
+			<el-button @click="cancel">返回</el-button>
+		</div>
+	</el-dialog>
 </template>
 <script>
-import ImageUpload from '../CommonComponents/ImageUpload'
+
 export default {
-    props: {
-        visible: {
-            type: Boolean,
-            default: false
-        }
-    },
-    data() {
-        return {
-            photos: {}
-        }
-    },
-    components: {
-        ImageUpload
-    },
-    created() {
-    },
-    methods: {
-        cancel() {
-            this.$emit('callback', false)
-        }
-    }
+	props: {
+		visible: {
+			type: Boolean,
+			default: false
+		}
+	},
+	data() {
+		return {
+			photos: {}
+		}
+	},
+	created() {
+	},
+	methods: {
+		cancel() {
+			this.$emit('callback', false)
+		}
+	}
 }
 </script>
 <style lang="stylus" scoped>
