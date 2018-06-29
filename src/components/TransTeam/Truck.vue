77e1ad543d0cad446ea9e0cfe45fc82a20f79baa
coolia <gj@vfu56.com>
2018-06-29 17:59:04 +0800

    ccccc

--- src/components/TransTeam/Truck.vue
+++ src/components/TransTeam/Truck.vue
@@ -4,34 +4,11 @@
 			<div class="header clearfix">车辆</div>
 			<div class="search">
 				<el-form :inline="true"  class="demo-form-inline"  size="small" :model="find" :rules="rules" ref="ruleForm">
-					<el-form-item label="车牌号" prop="plateNo">
-						<el-input placeholder="请输入..." v-model="find.plateNo"></el-input>
+					<el-form-item label="车辆" prop="plateNo">
+						<el-input placeholder="请输入车牌号" v-model="find.plateNo"></el-input>
 					</el-form-item>
-					<el-form-item label="自编号" prop="code">
-						<el-input placeholder="请输入..." v-model="find.code"></el-input>
-					</el-form-item>
-					<el-form-item label="牵引质量" prop="tractiveTonnage">
-						<el-input placeholder="请输入..." v-model="find.tractiveTonnage"><template slot="append">千克</template></el-input>
-					</el-form-item>
-					<el-form-item label="车长">
-						<el-input placeholder="请输入..." v-model="find.length"><template slot="append">毫米</template></el-input>
-					</el-form-item>
-					<el-form-item label="车宽">
-						<el-input placeholder="请输入..." v-model="find.width"><template slot="append">毫米</template></el-input>
-					</el-form-item>
-					<el-form-item label="车高">
-						<el-input placeholder="请输入..." v-model="find.high"><template slot="append">毫米</template></el-input>
-					</el-form-item>
-					<el-form-item label="添加时间">
-						<el-date-picker
-							v-model="find.date"
-							type="daterange"
-							range-separator="至"
-							start-placeholder="开始日期"
-							end-placeholder="结束日期" 
-							value-format="timestamp" 
-							@change="selectDateRange">
-						</el-date-picker>
+					<el-form-item label="司机" prop="code">
+						<el-input placeholder="请输入姓名或手机号" v-model="find.code"></el-input>
 					</el-form-item>
 					<el-form-item>
 						<el-button type="primary" @click="search">查询</el-button>
@@ -54,115 +31,23 @@
 					<el-button type="default" size="mini" icon="el-icon-upload2">导入</el-button>
 				</el-upload>
 				<a :href="templateUrl" :download="templateTit" class="download-btn"><svg-icon iconClass="excel-icon"></svg-icon> 下载模板</a>
-				<el-button type="default" size="mini" icon="el-icon-delete" @click="del">批量删除</el-button>
 			</div>
-			<div class="table">
-				<el-table 
-					ref="recTable" 
-					:data="tableData" 
-					@selection-change="selectionChange" 
-					border style="width: 100%" size="mini" stripe>
-					<el-table-column label="id" fixed type="selection" align="center" width="40"></el-table-column>
-					<el-table-column label="所属地区" prop="area" width="140">
-						<template slot-scope="scope">
-							<span v-if="scope.row.areaID">{{scope.row.areaID | searchAreaByKey()}}</span>
-						</template>
-					</el-table-column>
-					<el-table-column label="所属企业" prop="companyName"></el-table-column>
-					<el-table-column label="自编号" prop="code"></el-table-column>
-					<el-table-column label="车牌号" prop="plateNo"></el-table-column>
-					<el-table-column label="车牌颜色" prop="plateNoColor"></el-table-column>
-					<el-table-column label="车辆类别" prop="truckCategory">
-					</el-table-column>
-					<el-table-column label="车辆类型" prop="truckType"></el-table-column>
-					<el-table-column label="道路运输证号" prop="roadTransportNo" width="120"></el-table-column>
-					<el-table-column label="经营范围" prop="businessScope"></el-table-column>
-					<el-table-column label="道路运输证年审期至" width="130">
-						<template slot-scope="scope">
-							<span v-if="scope.row.roadTransportLicAnnualPeriod">{{scope.row.roadTransportLicAnnualPeriod | getdatefromtimestamp(true)}}</span>
-						</template>
-					</el-table-column>
-					<el-table-column label="行驶证审验效期至" prop="driverLicExamineExpires" width="120">
-						<template slot-scope="scope">
-							<span v-if="scope.row.driverLicExamineExpires">{{scope.row.driverLicExamineExpires | getdatefromtimestamp(true)}}</span>
-						</template>
-					</el-table-column>
-					<el-table-column label="承运人责任险有效期至" width="150">
-						<template slot-scope="scope">
-							<span v-if="scope.row.carrierRiskInsuranceExpires">{{scope.row.carrierRiskInsuranceExpires | getdatefromtimestamp(true)}}</span>
-						</template>
-					</el-table-column>
-					<el-table-column label="等级评定" prop="rank" width="100"></el-table-column>
-					<el-table-column label="下次等评日期" width="100">
-						<template slot-scope="scope">
-							<span v-if="scope.row.nextRankEvaluteTime">{{scope.row.nextRankEvaluteTime | getdatefromtimestamp(true)}}</span>
-						</template>
-					</el-table-column>
-					<el-table-column label="二级维护日期" width="100">
-						<template slot-scope="scope">
-							<span v-if="scope.row.secondaMaintainTime">{{scope.row.secondaMaintainTime | getdatefromtimestamp(true)}}</span>
-						</template>
-					</el-table-column>
-					<el-table-column label="下次二级维护日期" width="120">
-						<template slot-scope="scope">
-							<span v-if="scope.row.nextSecondLevel">{{scope.row.nextSecondLevel | getdatefromtimestamp(true)}}</span>
-						</template>
-					</el-table-column>
-					<el-table-column label="载重" prop="loads" width="100"></el-table-column>
-					<el-table-column label="罐体类型" prop="cannedType" width="100"></el-table-column>
-					<el-table-column label="罐体容积" prop="tankVolume" width="100"></el-table-column>
-					<el-table-column label="罐体检测有效期至" width="120">
-						<template slot-scope="scope">
-							<span v-if="scope.row.tankQCExpires">{{scope.row.tankQCExpires | getdatefromtimestamp(true)}}</span>
-						</template>
-					</el-table-column>
-					<el-table-column label="安全阀检测有效期至" width="130">
-						<template slot-scope="scope">
-							<span v-if="scope.row.safetyValvesQCExpires">{{scope.row.safetyValvesQCExpires | getdatefromtimestamp(true)}}</span>
-						</template>
-					</el-table-column>
-					<el-table-column label="压力表检测有效期至" width="130">
-						<template slot-scope="scope">
-							<span v-if="scope.row.pressureGaugeQCExpires">{{scope.row.pressureGaugeQCExpires | getdatefromtimestamp(true)}}</span>
-						</template>
-					</el-table-column>
-					<el-table-column label="挂车车牌" prop="trailerPlateNo" width="100"></el-table-column>
-					<el-table-column label="汽车生产厂家" prop="manufacturer" width="100"></el-table-column>
-					<el-table-column label="品牌型号" prop="carBrandModel" width="100"></el-table-column>
-					<el-table-column label="发动机号" prop="engineNO" width="100"></el-table-column>
-					<el-table-column label="车架号" prop="vehicleFrameNO" width="100"></el-table-column>
-					<el-table-column label="行驶证注册日期" width="120">
-						<template slot-scope="scope">
-							<span v-if="scope.row.driverLicRegisterTime">{{scope.row.driverLicRegisterTime | getdatefromtimestamp(true)}}</span>
-						</template>
-					</el-table-column>
-					<el-table-column label="行驶证发证日期" width="120">
-						<template slot-scope="scope">
-							<span v-if="scope.row.driverLicIssueTime">{{scope.row.driverLicIssueTime | getdatefromtimestamp(true)}}</span>
-						</template>
-					</el-table-column>
-					<el-table-column label="牵引质量" prop="tractiveTonnage" width="100"></el-table-column>
-					<el-table-column label="车长" prop="length" width="100"></el-table-column>
-					<el-table-column label="车宽" prop="width" width="100"></el-table-column>
-					<el-table-column label="车高" prop="high" width="100"></el-table-column>
-					<el-table-column label="添加时间" width="140">
-						<template slot-scope="scope">
-							<span v-if="scope.row.createTime">{{scope.row.createTime | getdatefromtimestamp()}}</span>
-						</template>
-					</el-table-column>
-					<el-table-column label="操作" width="80" align="center" fixed="right">
-						<template slot-scope="scope">
-							<el-dropdown  @command="handleCommand"  trigger="click">
-								<el-button type="primary" size="mini">操作<i class="el-icon-arrow-down el-icon--right"></i></el-button>
-								<el-dropdown-menu slot="dropdown">
-									<el-dropdown-item :command="{type: 'view', id: scope.row.truckID}" icon="el-icon-view">查看</el-dropdown-item>
-									<el-dropdown-item :command="{type: 'edit', id: scope.row.truckID}">编辑</el-dropdown-item>
-									<el-dropdown-item :command="{type: 'delete', id: scope.row.truckID}">删除</el-dropdown-item>
-								</el-dropdown-menu>
-							</el-dropdown>
-						</template>
-					</el-table-column>
-				</el-table>
+			<div class="driverList">
+				<div class="tableTit">
+					<div class="truckInfo">
+						车辆
+					</div>
+					<div class="driver">
+						驾驶员
+					</div>
+					<div class="mobile">
+						电话
+					</div>
+					<div class="handle">
+						操作
+					</div>
+
+				</div>
 				<Page :total="count" :pageSize="pageSize" @pageChange="pageChange" @pageSizeChange="pageSizeChange"/>
 			</div>
 		</div>
@@ -329,4 +214,33 @@ export default {
 .upload-File
 	display inline-block
 	margin 0 10px
+.driverList
+	.tableTit
+		background #409EFF
+		color #fff
+		height 36px
+		line-height 36px
+		display flex
+		text-align center
+		position relative
+		padding-right 520px
+		border-radius 3px
+		font-size 14px
+		font-weight bold
+		.driver
+		.mobile
+		.handle
+			border-left 1px solid #fff
+			position absolute
+	.truckInfo
+		width 100%
+	.handle
+		width 180px
+		right 0
+	.driver
+		width 200px
+		right 320px
+	.mobile
+		width 140px
+		right 180px
 </style>
