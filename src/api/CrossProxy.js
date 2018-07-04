8defc111247b09efea944ea0a0648258e9760912
Caoanlong <caoanlong@gmail.com>
2018-07-04 09:23:56 +0800

    添加elem定位

---
+++ src/api/CrossProxy.js
@@ -0,0 +1,20 @@
+import request from '../common/request'
+
+class CrossProxy {
+    constructor(baseUrl, request) {
+        this.baseUrl = baseUrl
+        this.request = request
+    }
+    getEleLocation(params) {
+        return new Promise((resolve, reject) => {
+            this.request({
+                url: this.baseUrl + '/ele/nearby',
+                params
+            }).then(res => {
+                resolve(res.data.data)
+            })
+        })
+    }
+}
+
+export default new CrossProxy('/cross/proxy', request)
