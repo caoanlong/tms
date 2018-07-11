9df71168d9ada7d12745e482ae32827ae4c95697
Caoanlong <caoanlong@gmail.com>
2018-07-11 10:15:58 +0800

    gg

--- src/components/Buisiness/Carrierbill/View.vue
+++ src/components/Buisiness/Carrierbill/View.vue
@@ -6,7 +6,6 @@
 				<el-tag size="mini" class="statusTag" v-else-if="carrierOrder.status=='Running'">执行中</el-tag>
 				<el-tag size="mini" class="statusTag" type="success" v-else-if="carrierOrder.status=='Signed'">已完成</el-tag>
 				<el-tag size="mini" class="statusTag" type="info" v-else-if="carrierOrder.status=='Closed'">已关闭</el-tag>
-				<!-- <el-tag size="mini" class="statusTag" type="info" v-else-if="carrierOrder.status=='Canceled'">作废</el-tag> -->
 			</div>
 			<div class="datetime">
 				<span class="label">发货单号：</span><span>{{carrierOrder.shipperNo}}</span>
@@ -15,147 +14,110 @@
 				<span v-if="carrierOrder.commissionDate">{{carrierOrder.commissionDate | getdatefromtimestamp(true)}}</span>
 				<span v-else></span>
 			</div>
-			<table class="wf-table">
-				<caption>收发货信息</caption>
-				<tr>
-					<td width="50%">
-						<span class="justify">发货单位</span>
-						{{carrierOrder.shipperCompanyName}}
-					</td>
-					<td width="50%">
-						<span class="justify">收货单位</span>
-						{{carrierOrder.consigneeCompanyName}}
-					</td>
-				</tr>
-				<tr>
-					<td>
-						<span class="justify">发货人</span>
-						{{carrierOrder.shipperName}}
-					</td>
-					<td>
-						<span class="justify">收货人</span>
-						{{carrierOrder.consigneeName}}
-					</td>
-				</tr>
-				<tr>
-					<td>
-						<span class="justify">联系方式</span>
-						{{carrierOrder.shipperPhone}}
-					</td>
-					<td>
-						<span class="justify">联系方式</span>
-						{{carrierOrder.consigneePhone}}
-					</td>
-				</tr>
-				<tr>
-					<td>
-						<span class="justify">发货地</span>
-						{{carrierOrder.shipperArea}}
-					</td>
-					<td>
-						<span class="justify">收货地</span>
-						{{carrierOrder.consigneeArea}}
-					</td>
-				</tr>
-				<tr>
-					<td>
-						<span class="justify">详细地址</span>
-						{{carrierOrder.shipperDetailAddress}}
-					</td>
-					<td>
-						<span class="justify">详细地址</span>
-						{{carrierOrder.consigneeDetailAddress}}
-					</td>
-				</tr>
-				<tr>
-					<td>
-						<span class="justify">发货时间</span>
-						<span v-if="carrierOrder.shipperDate">{{carrierOrder.shipperDate | getdatefromtimestamp(true)}}</span>
-					</td>
-					<td>
-						<span class="justify">到货时间</span>
-						<span v-if="carrierOrder.consigneeDate">{{carrierOrder.consigneeDate | getdatefromtimestamp(true)}}</span>
-					</td>
-				</tr>
-			</table>
-			<table class="wf-table">
-				<caption>货物信息</caption>
-				<tr>
-					<td colspan="7">
-						<span class="labels">运输方式：</span>{{carrierOrder.transportType}}
-					</td>
-				</tr>
-				<tr>
-					<th>货物类型</th>
-					<th>货物规格</th>
-					<th>货物名称</th>
-					<th>数量(件)</th>
-					<th>体积(m³)</th>
-					<th>重量(kg)</th>
-					<th>剩余货量</th>
-				</tr>
-				<tr class="is-center" v-for="item in carrierCargo" :key="item.carrierCargoID">
-					<td>
-						<span v-if="item.weightType=='Heavy'">重货</span>
-						<span v-else-if="item.weightType=='Light'">轻货</span>
-						<span v-else-if="item.weightType=='HeavyAndLight'">重轻货</span>
-					</td>
-					<td>{{item.cargoType}}</td>
-					<td>{{item.cargoName}}</td>
-					<td>{{item.cargoNum}}</td>
-					<td>{{item.cargoVolume}}</td>
-					<td>{{item.cargoWeight}}</td>
-					<td>
-						{{item.remainingCargoNum ? item.remainingCargoNum + '件' : ''}}
-						{{item.remainingCargoVolume ? ('/' + item.remainingCargoVolume + 'm³') : ''}}
-						{{item.remainingCargoWeight ? ('/' + item.remainingCargoWeight + 'kg') : ''}}
-					</td>
-				</tr>
-				<tr class="total is-center">
-					<td>合计</td>
-					<td colspan="2"></td>
-					<td>{{sum('cargoNum')}}</td>
-					<td>{{sum('cargoVolume')}}</td>
-					<td>{{sum('cargoWeight')}}</td>
-					<td>
-						<span v-if="sum('remainingCargoNum')>0">{{sum('remainingCargoNum')}}件/</span>
-						<span v-if="sum('remainingCargoVolume')>0">{{sum('remainingCargoVolume')}}m³/</span>
-						<span v-if="sum('remainingCargoWeight')>0">{{sum('remainingCargoWeight')}}kg</span>	
-					</td>
-				</tr>
-			</table>
-			<table class="wf-table">
-				<caption>运输进展</caption>
-				<tr>
-					<th>任务单号</th>
-					<th>车牌号</th>
-					<th>司机</th>
-					<th>押运员</th>
-					<th>货物名称</th>
-					<th>配载货量</th>
-					<th>运输照片</th>
-				</tr>
-				<tr class="is-center" v-for="(transport, index) in transports" :key="index">
-					<td><span class="link" @click="viewTask(transport.dispatchTaskID)">{{transport.taskNo}}</span></td>
-					<td>
-						{{transport.plateNo}}
-						{{transport.trailerPlateNo ? ('/' + transport.trailerPlateNo) : ''}}
-					</td>
-					<td>{{transport.driverName}}</td>
-					<td>{{transport.superCargoName}}</td>
-					<td>{{transport.cargoName}}</td>
-					<td>{{transport.loadWeightSum + 'kg'}}/{{transport.loadVolumeSum + 'm³'}}/{{transport.LoadNumSum + '件'}}</td>
-					<td style="text-align: center">
-						<el-button 
-							:disabled="transport.taskPicNum == 0"
-							type="primary" 
-							size="mini" 
-							@click="viewPhotos(transport.dispatchTaskID)">
-							查看({{transport.taskPicNum}})
-						</el-button>
-					</td>
-				</tr>
-			</table>
+			<el-row class="section-block" style="margin-bottom:20px;margin-top:20px;min-width: 1100px">
+				<span class="block-title">收发货信息</span>
+				<div class="block-content">
+					<el-row style="margin-bottom: 10px;">
+						<el-col :span="12">
+							<div><span class="tit">发货单位</span><span class="ctt">{{carrierOrder.shipperCompanyName}}</span></div>
+							<div><span class="tit">发货人</span><span class="ctt">{{carrierOrder.shipperName}}</span></div>
+							<div><span class="tit">联系方式</span><span class="ctt">{{carrierOrder.shipperPhone}}</span></div>
+							<div><span class="tit">发货地</span><span class="ctt">{{carrierOrder.shipperArea}}</span></div>
+							<div><span class="tit">详细地址</span><span class="ctt">{{carrierOrder.shipperDetailAddress}}</span></div>
+							<div><span class="tit">发货时间</span><span class="ctt"><span v-if="carrierOrder.shipperDate">{{carrierOrder.shipperDate | getdatefromtimestamp(true)}}</span></span></div>
+						</el-col>
+						<el-col :span="12">
+							<div><span class="tit">收货单位</span><span class="ctt">{{carrierOrder.consigneeCompanyName}}</span></div>
+							<div><span class="tit">收货人</span><span class="ctt">{{carrierOrder.consigneeName}}</span></div>
+							<div><span class="tit">联系方式</span><span class="ctt">{{carrierOrder.consigneePhone}}</span></div>
+							<div><span class="tit">收货地</span><span class="ctt">{{carrierOrder.consigneeArea}}</span></div>
+							<div><span class="tit">详细地址</span><span class="ctt">{{carrierOrder.consigneeDetailAddress}}</span></div>
+							<div><span class="tit">到货时间</span><span class="ctt"><span v-if="carrierOrder.consigneeDate">{{carrierOrder.consigneeDate | getdatefromtimestamp(true)}}</span></span></div>
+						</el-col>
+					</el-row>
+				</div>
+			</el-row>
+			<el-row class="section-block" style="margin-bottom:20px;">
+				<span class="block-title">货物信息</span>
+				<div class="block-content">
+					<table class="wf-table">
+						<tr>
+							<th>货物类型</th>
+							<th>货物规格</th>
+							<th>货物名称</th>
+							<th>数量(件)</th>
+							<th>体积(m³)</th>
+							<th>重量(kg)</th>
+							<th>剩余货量</th>
+						</tr>
+						<tr class="is-center" v-for="item in carrierCargo" :key="item.carrierCargoID">
+							<td>
+								<span v-if="item.weightType=='Heavy'">重货</span>
+								<span v-else-if="item.weightType=='Light'">轻货</span>
+								<span v-else-if="item.weightType=='HeavyAndLight'">重轻货</span>
+							</td>
+							<td>{{item.cargoType}}</td>
+							<td>{{item.cargoName}}</td>
+							<td>{{item.cargoNum}}</td>
+							<td>{{item.cargoVolume}}</td>
+							<td>{{item.cargoWeight}}</td>
+							<td>
+								{{item.remainingCargoNum ? item.remainingCargoNum + '件' : ''}}
+								{{item.remainingCargoVolume ? ('/' + item.remainingCargoVolume + 'm³') : ''}}
+								{{item.remainingCargoWeight ? ('/' + item.remainingCargoWeight + 'kg') : ''}}
+							</td>
+						</tr>
+						<tr class="total is-center">
+							<td>合计</td>
+							<td colspan="2"></td>
+							<td>{{sum('cargoNum')}}</td>
+							<td>{{sum('cargoVolume')}}</td>
+							<td>{{sum('cargoWeight')}}</td>
+							<td>
+								<span v-if="sum('remainingCargoNum')>0">{{sum('remainingCargoNum')}}件/</span>
+								<span v-if="sum('remainingCargoVolume')>0">{{sum('remainingCargoVolume')}}m³/</span>
+								<span v-if="sum('remainingCargoWeight')>0">{{sum('remainingCargoWeight')}}kg</span>	
+							</td>
+						</tr>
+					</table>
+				</div>
+			</el-row>
+			<el-row class="section-block" style="margin-bottom:20px;">
+				<span class="block-title">运输进展</span>
+				<div class="block-content">
+					<table class="wf-table">
+						<tr>
+							<th>任务单号</th>
+							<th>车牌号</th>
+							<th>司机</th>
+							<th>押运员</th>
+							<th>货物名称</th>
+							<th>配载货量</th>
+							<th>运输照片</th>
+						</tr>
+						<tr class="is-center" v-for="(transport, index) in transports" :key="index">
+							<td><span class="link" @click="viewTask(transport.dispatchTaskID)">{{transport.taskNo}}</span></td>
+							<td>
+								{{transport.plateNo}}
+								{{transport.trailerPlateNo ? ('/' + transport.trailerPlateNo) : ''}}
+							</td>
+							<td>{{transport.driverName}}</td>
+							<td>{{transport.superCargoName}}</td>
+							<td>{{transport.cargoName}}</td>
+							<td>{{transport.loadWeightSum + 'kg'}}/{{transport.loadVolumeSum + 'm³'}}/{{transport.LoadNumSum + '件'}}</td>
+							<td style="text-align: center">
+								<el-button 
+									:disabled="transport.taskPicNum == 0"
+									type="primary" 
+									size="mini" 
+									@click="viewPhotos(transport.dispatchTaskID)">
+									查看({{transport.taskPicNum}})
+								</el-button>
+							</td>
+						</tr>
+					</table>
+				</div>
+			</el-row>
 			<div class="wf-footer clearfix text-center">
 				<el-button @click="back">返回</el-button>
 			</div>
@@ -168,18 +130,12 @@
 			:shipperArea="currentShipperArea" 
 			:consigneeArea="currentConsigneeArea">
 		</UploadPhoto>
-		<ViewCargos 
-			:visible="dialogCargoVisible" 
-			@callback="cargoCallback">
-		</ViewCargos>
 	</div>
 </template>
 <script type="text/javascript">
 import { Message } from 'element-ui'
-import { closeConfirm, deleteConfirm } from '../../../common/utils'
 import Carrierbill from '../../../api/Carrierbill'
 import UploadPhoto from '../../Dispatch/DispatchBill/components/UploadPhoto'
-import ViewCargos from './components/ViewCargos'
 export default {
 	data() {
 		return {
@@ -188,18 +144,14 @@ export default {
 			currentDispatchTaskID: '',
 			currentShipperArea: '',
 			currentConsigneeArea: '',
-			dialogCargoVisible: false,
 			carrierCargo: [],
 			porRequire: [],
 			transports: []
 		}
 	},
-	components: {
-		UploadPhoto,
-		ViewCargos
-	},
+	components: { UploadPhoto },
 	created() {
-		this.getDetail()
+		this.getInfo()
 	},
 	methods: {
 		sum(o) {
@@ -209,8 +161,8 @@ export default {
 			}
 			return sum
 		},
-		getDetail() {
-			let carrierOrderID = this.$route.query.carrierOrderID
+		getInfo() {
+			const carrierOrderID = this.$route.query.carrierOrderID
 			Carrierbill.findById({ carrierOrderID }).then(res => {
 				this.carrierOrder = res
 				this.carrierCargo = res.carrierCargo
@@ -234,10 +186,6 @@ export default {
 		handUploadPhoto(bool) {
 			this.isPhotoVisible = false
 		},
-		// 查看货物弹窗回调
-		cargoCallback(bool) {
-			this.dialogCargoVisible = bool
-		},
 		viewTask(dispatchTaskID) {
 			this.$router.push({name: 'viewtaskdetail', query: { dispatchTaskID, type: 'view', isHideAmount: true }})
 		},
@@ -248,20 +196,34 @@ export default {
 }
 </script>
 <style lang="stylus" scoped>
-	.statusTag
-		position absolute
-		right 10px
-		top 50%
-		transform translateY(-50%)
-	.datetime
-		font-size 12px
-		padding 10px 15px 
-		margin-bottom 10px
-		border-bottom 1px solid #e2ecf6
-		color #3582d0
-		.label~.label
-			margin-left 40px				
+.statusTag
+	position absolute
+	right 10px
+	top 50%
+	transform translateY(-50%)
+.datetime
+	font-size 12px
+	padding 10px 15px 
+	margin-bottom 10px
+	border-bottom 1px solid #e2ecf6
+	color #3582d0
+	.label~.label
+		margin-left 40px				
 .porRequire
 	span
-		margin-right 20px				
+		margin-right 20px
+.block-content
+	font-size 14px
+	line-height 2
+	.tit
+		display inline-block
+		width 126px
+		text-align right
+		margin-right 10px
+		color #333
+		font-weight bold
+	.ctt
+		display inline-block
+		text-align left
+		color #666
 </style>
