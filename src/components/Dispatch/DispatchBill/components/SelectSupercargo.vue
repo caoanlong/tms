95497037eced40842a71e1c2fb40c69bb453e76a
coolia <gj@vfu56.com>
2018-08-02 11:51:07 +0800

    cccc

--- src/components/Dispatch/DispatchBill/components/SelectSupercargo.vue
+++ src/components/Dispatch/DispatchBill/components/SelectSupercargo.vue
@@ -49,6 +49,14 @@
                     </span>
                     <el-tag type="success" size="mini" v-if="scope.row.isPrimary == 'Y'">主驾</el-tag>
                     <el-tag type="success" size="mini" v-else-if="scope.row.isSecondary == 'Y'">副驾</el-tag>
+                    <el-tooltip placement="right" effect="light" popper-class="expirewarnPop">
+                        <div slot="content">
+                            <el-tag size="mini" type="danger" v-for="(item,index) in scope.row.expiredCertificate" :key="index">{{
+                                expireWarnJson[item]
+                            }}</el-tag>
+                        </div>
+                        <el-tag size="mini" type="danger" v-if="scope.row.expiredCertificate.length>0">到期</el-tag>
+                    </el-tooltip>
                 </template>
             </el-table-column>
             <el-table-column label="手机号" prop="mobile" align="center"></el-table-column>
@@ -89,7 +97,30 @@ export default {
             pageIndex: 1,
 			pageSize: 10,
 			total: 0,
-            superCargos: []
+            superCargos: [],
+            expireWarnJson:{
+                DriverLicExpiresTime:"行驶证到期",
+                RoadTransportLicAnnualPeriod:"道路运输证到期 ",
+                GpsValidEndDate:"GPS到期 ",
+                SaliInsuranceExpires:"交强险到期 ",
+                BizInsuranceExpires:"商业险到期 ",
+                CarrierRiskInsuranceExpires:"承运险到期 ",
+                CargoInsuranceExpireDate:"货运险到期 ",
+                TankQCExpires:"罐体监测到期 ",
+                SafetyValvesQCExpires:"罐体安全阀监测到期 ",
+                PressureGaugeQCExpires:"罐体压力表监测到期 ",
+                secondaMaintainTime:"二级维护下次维护日期到期 ",
+                NextRankEvaluteTime:"技术等级下次评定到期",
+                SecondSecurityDepositDate:"二次安全保证金到期 ",
+                ManagementAgreementExpireDate:"管理协议到期 ",
+                SafetyLiabilityLetterExpireDate:"安全责任书到期",
+                IdCardExpirationTime:"身份证到期 ",
+                DriverLicenseEndTime:"驾驶证到期 ",
+                QualificationExpirationDate:"危货从业资格证到期 ",
+                IntegrityExamineEndTime:"诚信考核证到期 ",
+                LaborContractEndTime:"聘用合同到期 ",
+                EscortLicenseExpireDate:"押运证到期"
+            }
         }
     },
     watch: {
@@ -156,8 +187,23 @@ export default {
                 appStatus: this.find.appStatus,
                 truckID: this.truck.truckID
 			}).then(res => {
+                const list = res.records.map(item => {
+                    return {
+                        appStatus: item.appStatus,
+                        comSupercargoID: item.comSupercargoID,
+                        mobile: item.mobile,
+                        headPic:item.headPic,
+                        isPrimary:item.isPrimary,
+                        isSecondary:item.isSecondary,
+                        realName:item.realName,
+                        supercargoType:item.supercargoType,
+                        workStatus: item.workStatus,
+                        expiredCertificate: item.expiredCertificate
+                        ? (item.expiredCertificate.indexOf(',') ? item.expiredCertificate.split(',') : [item.expiredCertificate]) : []
+                    }
+                })
                 this.total = res.total
-                this.superCargos = res.records
+                this.superCargos = list
 			})
         },
         getSuperCagoList() {
@@ -168,8 +214,20 @@ export default {
                 shipperDate: this.find.shipperDate,
                 workStatus: this.find.workStatus
 			}).then(res => {
+                const list = res.records.map(item => {
+                    return {
+                        comSupercargoID: item.comSupercargoID,
+                        mobile: item.mobile,
+                        headPic:item.headPic,
+                        realName:item.realName,
+                        supercargoType:item.supercargoType,
+                        workStatus: item.workStatus,
+                        expiredCertificate: item.expiredCertificate
+                        ? (item.expiredCertificate.indexOf(',') ? item.expiredCertificate.split(',') : [item.expiredCertificate]) : []
+                    }
+                })
                 this.total = res.total
-                this.superCargos = res.records
+                this.superCargos = list
 			})
 		},
         control(bool) {
