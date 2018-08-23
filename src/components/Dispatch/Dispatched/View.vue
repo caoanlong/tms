4948d517fc998c8ff45cb82dd23eb293c5055a31
coolia <gj@vfu56.com>
2018-08-23 12:16:34 +0800

    ccccc

--- src/components/Dispatch/Dispatched/View.vue
+++ src/components/Dispatch/Dispatched/View.vue
@@ -24,116 +24,118 @@
 						<span class="c1" v-if="dispatchOrderDetail.loadNumSum"><span class="num-label">数</span> {{dispatchOrderDetail.loadNumSum}}</span>
 					</p>
 					<p>运费<span class="fr c1 carriage" @click="carriageDetail">{{dispatchOrderDetail.sumAmount}}元 <svg-icon icon-class="arrow-down" :class="ShowCarriageDetail?'':'unfold'"></svg-icon></span></p>
-					<table class="wf-table" v-show="ShowCarriageDetail">
-                        <thead>
-                            <tr>
-                                <th>费用科目</th>
-                                <th width="80">费用类型</th>
-                                <th>收款人</th>
-                                <th>支付方式</th>
-                                <th>金额</th>
-                                <th width="190" v-if="!(dispatchOrderDetail.status=='Canceled') || !(dispatchOrderDetail.status=='Rejected')">
-									<el-button size="mini" type="primary" icon="el-icon-plus" @click="addFreight">添加</el-button>
-									<el-button size="mini" type="success" icon="el-icon-check" @click="saveFreight" v-if="bizDispatchFeeList.length">保存</el-button>
-								</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr v-for="feesItem in dispatchOrderFees" :key="feesItem.dispatchFeeID">
-                                <td>
-                                	<!-- 费用科目:Freight-运费   RoadBridge-路桥费  Transit-中转费  Fine-罚款  Detour-绕路费  Other-其他 -->
-                                	<span v-if="feesItem.item=='Freight'">运费</span>
-                                	<span v-else-if="feesItem.item=='RoadBridge'">路桥费</span>
-                                	<span v-else-if="feesItem.item=='Transit'">中转费</span>
-                                	<span v-else-if="feesItem.item=='Fine'">罚款</span>
-                                	<span v-else-if="feesItem.item=='Detour'">绕路费</span>
-                                	<span v-else>其他</span>
-                                </td>
-                                <td>
-                                    <span>{{feesItem.category == 'Basic' ? '基础运费' : '附加运费'}}</span>
-                                </td>
-                                <td>
-                                    {{feesItem.supercargoName}}
-                                </td>
-                                <td>
-                                    <!-- 支付方式: Prepay-预付  PayOnDelivery-到付  PayOnReceipt-回单结  PayByConsignee-收货方付 -->
-									<span v-if="feesItem.payMode=='Prepay'">预付</span>
-                                	<span v-else-if="feesItem.payMode=='PayOnDelivery'">到付</span>
-                                	<span v-else-if="feesItem.payMode=='PayOnReceipt'">回单结</span>
-                                	<span v-else>收货方付</span>
-                                </td>
-                                <td>
-                                    {{feesItem.amount}}元
-                                </td>
-                                <td v-if="!(dispatchOrderDetail.status=='Canceled') || !(dispatchOrderDetail.status=='Rejected')"></td>
-                            </tr>
-                            <tr v-for="(item, index) in bizDispatchFeeList" :key="index">
-                                <td>
-                                    <el-form :model="item" ref="ruleForm">
-                                        <el-form-item prop="item" :rules="[{ required: true , message: '请选择费用科目' }]">
-                                            <el-select size="mini" v-model="item.item" placeholder="请选择">
-                                                <el-option label="路桥费" value="RoadBridge"></el-option>
-                                                <el-option label="中转费" value="Transit"></el-option>
-                                                <el-option label="罚款" value="Fine"></el-option>
-                                                <el-option label="绕路费" value="Detour"></el-option>
-                                                <el-option label="其他" value="Other"></el-option>
-                                            </el-select>
-                                        </el-form-item>
-                                    </el-form>
-                                </td>
-                                <td>
-                                    <span style="position:relative;top:-10px">{{item.category == 'Basic' ? '基础运费' : '附加运费'}}</span>
-                                </td>
-                                <td>
-                                    <el-form :model="item" ref="ruleForm">
-                                        <el-form-item prop="superCargoID" :rules="[{ required: true , message: '请选择收款人' }]">
-                                            <el-select size="mini" value-key="supercargoID" v-model="item.superCargo" placeholder="请选择" @change="handSelectItem($event, index)">
-                                                <el-option 
-                                                    :label="person.realName" 
-                                                    :value="person" 
-                                                    v-for="person in persons" 
-                                                    :key="person.supercargoID">
-                                                </el-option>
-                                            </el-select>
-                                        </el-form-item>
-                                    </el-form>
-                                </td>
-                                <td>
-                                    <el-form :model="item" ref="ruleForm">
-                                        <el-form-item prop="payMode" :rules="[{ required: true , message: '请选择支付方式' }]">
-                                            <el-select size="mini" v-model="item.payMode" placeholder="请选择">
-                                                <el-option label="到付" value="PayOnDelivery"></el-option>
-                                                <el-option label="预付" value="Prepay"></el-option>
-                                                <el-option label="回单结" value="PayOnReceipt"></el-option>
-                                                <el-option label="收货方付" value="PayByConsignee"></el-option>
-                                            </el-select>
-                                        </el-form-item>
-                                    </el-form>
-                                </td>
-                                <td>
-                                    <el-form :model="item" ref="ruleForm">
-                                        <el-form-item prop="amount" :rules="[{
-												validator: (rule, value, callback) => {
-													const r = /\d+/
-													if (!item.amount || item.amount == '0') {
-														callback('请输入金额')
-													} else if (value && !r.test(value)) {
-														callback('请输入正确的数字')
-													} else {
-														callback()
+					<div class="tableBox">
+						<table class="wf-table" v-show="ShowCarriageDetail" style="min-width:750px">
+							<thead>
+								<tr>
+									<th width="110">费用科目</th>
+									<th width="90">费用类型</th>
+									<th width="110">收款人</th>
+									<th width="110">支付方式</th>
+									<th width="130">金额</th>
+									<th width="180" v-if="!(dispatchOrderDetail.status=='Canceled') || !(dispatchOrderDetail.status=='Rejected')">
+										<el-button size="mini" type="primary" icon="el-icon-plus" @click="addFreight">添加</el-button>
+										<el-button size="mini" type="success" icon="el-icon-check" @click="saveFreight" v-if="bizDispatchFeeList.length">保存</el-button>
+									</th>
+								</tr>
+							</thead>
+							<tbody>
+								<tr v-for="feesItem in dispatchOrderFees" :key="feesItem.dispatchFeeID">
+									<td>
+										<!-- 费用科目:Freight-运费   RoadBridge-路桥费  Transit-中转费  Fine-罚款  Detour-绕路费  Other-其他 -->
+										<span v-if="feesItem.item=='Freight'">运费</span>
+										<span v-else-if="feesItem.item=='RoadBridge'">路桥费</span>
+										<span v-else-if="feesItem.item=='Transit'">中转费</span>
+										<span v-else-if="feesItem.item=='Fine'">罚款</span>
+										<span v-else-if="feesItem.item=='Detour'">绕路费</span>
+										<span v-else>其他</span>
+									</td>
+									<td>
+										<span>{{feesItem.category == 'Basic' ? '基础运费' : '附加运费'}}</span>
+									</td>
+									<td>
+										{{feesItem.supercargoName}}
+									</td>
+									<td>
+										<!-- 支付方式: Prepay-预付  PayOnDelivery-到付  PayOnReceipt-回单结  PayByConsignee-收货方付 -->
+										<span v-if="feesItem.payMode=='Prepay'">预付</span>
+										<span v-else-if="feesItem.payMode=='PayOnDelivery'">到付</span>
+										<span v-else-if="feesItem.payMode=='PayOnReceipt'">回单结</span>
+										<span v-else>收货方付</span>
+									</td>
+									<td>
+										{{feesItem.amount}}元
+									</td>
+									<td v-if="!(dispatchOrderDetail.status=='Canceled') || !(dispatchOrderDetail.status=='Rejected')"></td>
+								</tr>
+								<tr v-for="(item, index) in bizDispatchFeeList" :key="index">
+									<td>
+										<el-form :model="item" ref="ruleForm">
+											<el-form-item prop="item" :rules="[{ required: true , message: '请选择费用科目' }]">
+												<el-select size="mini" v-model="item.item" placeholder="请选择"  style="width:100%">
+													<el-option label="路桥费" value="RoadBridge"></el-option>
+													<el-option label="中转费" value="Transit"></el-option>
+													<el-option label="罚款" value="Fine"></el-option>
+													<el-option label="绕路费" value="Detour"></el-option>
+													<el-option label="其他" value="Other"></el-option>
+												</el-select>
+											</el-form-item>
+										</el-form>
+									</td>
+									<td>
+										<span style="position:relative;top:-10px">{{item.category == 'Basic' ? '基础运费' : '附加运费'}}</span>
+									</td>
+									<td>
+										<el-form :model="item" ref="ruleForm">
+											<el-form-item prop="superCargoID" :rules="[{ required: true , message: '请选择收款人' }]">
+												<el-select size="mini" value-key="supercargoID" v-model="item.superCargo" placeholder="请选择" @change="handSelectItem($event, index)">
+													<el-option 
+														:label="person.realName" 
+														:value="person" 
+														v-for="person in persons" 
+														:key="person.supercargoID">
+													</el-option>
+												</el-select>
+											</el-form-item>
+										</el-form>
+									</td>
+									<td>
+										<el-form :model="item" ref="ruleForm">
+											<el-form-item prop="payMode" :rules="[{ required: true , message: '请选择支付方式' }]">
+												<el-select size="mini" v-model="item.payMode" placeholder="请选择" style="width:100%">
+													<el-option label="到付" value="PayOnDelivery"></el-option>
+													<el-option label="预付" value="Prepay"></el-option>
+													<el-option label="回单结" value="PayOnReceipt"></el-option>
+													<el-option label="收货方付" value="PayByConsignee"></el-option>
+												</el-select>
+											</el-form-item>
+										</el-form>
+									</td>
+									<td>
+										<el-form :model="item" ref="ruleForm">
+											<el-form-item prop="amount" :rules="[{
+													validator: (rule, value, callback) => {
+														const r = /\d+/
+														if (!item.amount || item.amount == '0') {
+															callback('请输入金额')
+														} else if (value && !r.test(value)) {
+															callback('请输入正确的数字')
+														} else {
+															callback()
+														}
 													}
-												}
-											}]">
-                                            <el-input size="mini" placeholder="请输入..." v-model="item.amount" style="border-spacing:0"><template slot="append">元</template></el-input>
-                                        </el-form-item>
-                                    </el-form>
-                                </td>
-                                <td>
-                                    <span style="position:relative;top:-10px" class="delbtn c6" @click="delFreight(index)">删除</span>
-                                </td>
-                            </tr>
-                        </tbody>
-                    </table>
+												}]">
+												<el-input size="mini" placeholder="请输入..." v-model="item.amount" style="border-spacing:0;width:100%"><template slot="append">元</template></el-input>
+											</el-form-item>
+										</el-form>
+									</td>
+									<td>
+										<span style="position:relative;top:-10px" class="delbtn c6" @click="delFreight(index)">删除</span>
+									</td>
+								</tr>
+							</tbody>
+						</table>
+					</div>
 					<p>运输任务（<span class="circle"></span> 色数字代表装卸执行顺序，来源线路规划排序） <span class="fr c1" v-if="dispatchOrderDetail.distance">预计总里程{{Number(dispatchOrderDetail.distance/1000).toFixed(2)}}公里</span></p>
 					<TaskItem v-for="(item,index) in dispatchTask" :taskItem="item" :index="index" :key="item.carrierOrderID"></TaskItem>
 				</el-col>
@@ -226,54 +228,68 @@ export default {
 			this.$router.go(-1)
 		},
 		createPersons() {
-            if (this.dispatchOrderDetail.driverID) {
-                this.persons.push({
-                	supercargoID:this.dispatchOrderDetail.driverID,
+			if (this.dispatchOrderDetail.driverID) {
+				this.persons.push({
+					supercargoID:this.dispatchOrderDetail.driverID,
 					realName:this.dispatchOrderDetail.driverName
 				})
 			}
 			if (this.dispatchOrderDetail.superCargoID) {
-                this.persons.push({
-                	supercargoID:this.dispatchOrderDetail.superCargoID,
+				this.persons.push({
+					supercargoID:this.dispatchOrderDetail.superCargoID,
 					realName:this.dispatchOrderDetail.superCargoName
 				})
-            }
-        },
+			}
+		},
 		carriageDetail(){
 			this.ShowCarriageDetail = !this.ShowCarriageDetail
 		},
 		addFreight() {
-            this.bizDispatchFeeList.push({
-                item: '',
-                category: 'Attach',
-                superCargoID: '',
-                payMode: '',
-                amount: ''
-            })
-        },
-        handSelectItem(data, index) {
-            this.bizDispatchFeeList[index].superCargoID = data.supercargoID
-            this.bizDispatchFeeList[index].supercargoName = data.realName
-        },
-        saveFreight(){
-        	const dispatchOrderID = this.$route.query.dispatchOrderID
-        	const bizDispatchFeeList = this.bizDispatchFeeList.map(item=>{
-        		return {
-        			item: item.item,
-		            category: item.category,
-		            superCargoID: item.superCargoID,
-		            payMode: item.payMode,
-		            supercargoName:item.supercargoName,
-		            amount: item.amount,
-		            dispatchOrderID
-        		}
-        	})
-        	Dispatchbill.feeModify({bizDispatchFeeList}).then(res=>{
-				this.bizDispatchFeeList = []
-        		Message.success(res.data.msg)
-        		this.getFees()
-        	})
-        }
+			this.bizDispatchFeeList.push({
+				item: '',
+				category: 'Attach',
+				superCargoID: '',
+				payMode: '',
+				amount: ''
+			})
+		},
+		delFreight(i) {
+			this.bizDispatchFeeList.splice(i, 1)
+		},
+		handSelectItem(data, index) {
+			this.bizDispatchFeeList[index].superCargoID = data.supercargoID
+			this.bizDispatchFeeList[index].supercargoName = data.realName
+		},
+		saveFreight(){
+			new Promise((resolve, reject) => {
+				let flag = true
+				for (let i = 0; i < this.$refs['ruleForm'].length; i++) {
+					const item = this.$refs['ruleForm'][i]
+					item.validate(valid => {
+						if (!valid) flag = false
+					})
+				}
+				flag ? resolve() : reject()
+			}).then(() => {
+				const dispatchOrderID = this.$route.query.dispatchOrderID
+				const bizDispatchFeeList = this.bizDispatchFeeList.map(item=>{
+					return {
+						item: item.item,
+						category: item.category,
+						superCargoID: item.superCargoID,
+						payMode: item.payMode,
+						supercargoName:item.supercargoName,
+						amount: item.amount,
+						dispatchOrderID
+					}
+				})
+				Dispatchbill.feeModify({bizDispatchFeeList}).then(res=>{
+					this.bizDispatchFeeList = []
+					Message.success(res.data.msg)
+					this.getFees()
+				})
+			}).catch(err => {})
+		}
 	},
 	components:{
 		TaskItem
@@ -304,7 +320,9 @@ export default {
 		.unfold
 			transform rotate(180deg)
 			transition all .5s
-			
+	.tableBox
+		overflow hidden
+		overflow-x auto	
 	.wf-table
 		td
 			text-align center
--- src/components/Dispatch/Dispatched/index.vue
+++ src/components/Dispatch/Dispatched/index.vue
@@ -86,7 +86,7 @@
 								<div class="handler">
 									<span class="c1" @click="scramble" v-if="item.grabNum>0&&item.type=='Grab'">抢单人数（{{item.grabNum}}）</span>
 									<el-button type="text" size="mini" @click="trail">跟踪</el-button>
-									<router-link tag="span" class="c1" :to="{name: 'redispatching', query: {dispatchOrderID: item.dispatchOrderID}}">重新调度</router-link>
+									
 									<!-- 未接单 -->
 									<span v-if="item.status == 'Committed'">
 										<el-button type="text" size="mini" :disabled="
@@ -105,7 +105,8 @@
 									</span>
 									<!-- 已取消 已拒绝-->
 									<span v-else>
-										<el-button type="text" size="mini" :disabled="true" @click="cancelDispatchOrder(item.dispatchOrderID)">取消调度</el-button>
+										<router-link tag="span" class="c6" style="margin-left:0" :to="{name: 'redispatching', query: {dispatchOrderID: item.dispatchOrderID}}">重新调度</router-link>
+										<!-- <el-button type="text" size="mini" :disabled="true" @click="cancelDispatchOrder(item.dispatchOrderID)">取消调度</el-button> -->
 										<el-button type="text" size="mini" :disabled="true"  @click="closeDispatchOrder(item.dispatchOrderID)" style="margin-left:20px">关闭</el-button>
 									</span>
 								</div>
