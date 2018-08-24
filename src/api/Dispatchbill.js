f421fbdfbed81003c4e126f5ed2495843e70b91d
coolia <gj@vfu56.com>
2018-08-24 16:55:41 +0800

    ccccccc

--- src/api/Dispatchbill.js
+++ src/api/Dispatchbill.js
@@ -161,7 +161,24 @@ class Dispatchbill extends Base {
             })
         })
     }
-    
+    findgGrabOfferOrderList(params){
+        return new Promise((resolve, reject) => {
+            this.request({
+                url: this.baseUrl + '/grabOfferOrderList',
+                params
+            }).then(res => {
+                resolve(res.data.data)
+            })
+        })
+    }
+    // 调度单-抢单/报价-确认
+    confirmScramble(data){
+    	return this.request({
+            url: this.baseUrl + '/offer/determine',
+            method: 'post',
+            data
+        })
+    }
     //修改运费
 	feeModify(data){
         return requestByJson({
