947c770ea2b9376e7394a60bf492d5bdc9f9845e
coolia <gj@vfu56.com>
2018-08-27 15:26:37 +0800

    cccc

--- config/test.env.js
+++ config/test.env.js
@@ -1,9 +1,9 @@
 'use strict'
 module.exports = {
-	NODE_ENV: '"production"',
-	ENV_CONFIG: '"test"',
+    NODE_ENV: '"production"',
+    ENV_CONFIG: '"test"',
     BASE_API: '"http://develop.we-service.cn/tms-api"', // 测试环境
-    BASE_NODE_API: '"http://develop.we-service.cn/tms/nodejs/api"',   // node测试环境
+    BASE_NODE_API: '"http://develop.we-service.cn/tms/nodejs/api"', // node测试环境
     BASE_SYS_API: '"http://192.168.1.110:9919"', // 测试系统服务
-	IMG_API: '"http://develop.we-service.cn/hdd/image/"'  // 测试环境
+    IMG_API: '"http://develop.we-service.cn/hdd/image/"' // 测试环境
 }
