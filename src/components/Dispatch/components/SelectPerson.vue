b5fa35d2a52c6c028b8faea3e0ef9fc6f47b4dca
Caoanlong <caoanlong@gmail.com>
2018-09-14 16:36:38 +0800

    gg

--- src/components/Dispatch/components/SelectPerson.vue
+++ src/components/Dispatch/components/SelectPerson.vue
@@ -232,6 +232,7 @@ export default {
 		},
         close(bool) {
             if (bool) {
+                if (selected.supercargoID) return
                 this.$emit('control', this.type, this.selected)
             } else {
                 this.$emit('control', this.type)
