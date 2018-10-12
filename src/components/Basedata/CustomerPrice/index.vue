ca288a897d76fb5447a1abb3b39b823ad7d3f886
coolia <gj@vfu56.com>
2018-10-12 12:29:47 +0800

    ccccc

--- src/components/Basedata/CustomerPrice/index.vue
+++ src/components/Basedata/CustomerPrice/index.vue
@@ -13,6 +13,22 @@
 					</el-form-item>
 				</el-form>
 			</div>
+			<div class="tableControl">
+				<el-upload 
+					class="upload-File" 
+					name="excel" 
+					:action="importFileUrl" 
+					:auto-upload="true" 
+					:onError="uploadError" 
+					:onSuccess="uploadSuccess" 
+					:beforeUpload="beforeFileUpload" 
+					:headers="uploadHeaders" 
+					:show-file-list="false">
+					<el-button type="default" size="mini" icon="el-icon-upload2">导入</el-button>
+				</el-upload>
+				<a :href="exportExcelUrl" download="goodssource.xlsx" class="exportExcel el-icon-download">导出</a>
+				<a :href="templateUrl" download="trucksource.xlsx" class="download-btn"><svg-icon iconClass="excel-icon"></svg-icon> 下载模板</a>
+			</div>
 			<div class="table">
 				<el-table 
 					ref="recTable" 
@@ -134,10 +150,41 @@ export default {
         callbackLinePrice(bool) {
 			this.isLinePriceVisible = false
 			bool && this.getList()
-        }
+		},
+		// 导入成功
+		uploadSuccess (response) {
+			if(response.code != 200){
+				Message.error(response.msg)
+			} else{
+				Message.success(response.msg)
+				this.getList()
+			}
+		}
 	}
 }
 </script>
 <style lang="stylus" scoped>
-
+.download-btn
+.exportExcel
+	font-size 12px
+	color #606266
+	height 29px
+	line-height 29px
+	padding 0 15px
+	border 1px solid #dcdfe6
+	border-radius 3px
+	background #fff
+	margin-right 10px
+	display inline-block
+	vertical-align top
+	&:hover
+		border-color #c6e2ff
+		color #409eff
+		background #ecf5ff
+	&:active
+		border-color #3a8ee6
+		color #3a8ee6
+.upload-File
+	display inline-block
+	margin-right 10px
 </style>
