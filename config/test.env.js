dfefd0e71f6c316e5bd5b76fd13dcd31d6ac5e25
Caoanlong <caoanlong@gmail.com>
2018-11-01 10:59:48 +0800

    修改Api URL

--- config/test.env.js
+++ config/test.env.js
@@ -2,7 +2,7 @@
 module.exports = {
     NODE_ENV: '"production"',
     ENV_CONFIG: '"test"',
-    BASE_API: '"http://develop.we-service.cn/tms-api"', // 测试环境
+    BASE_API: '"http://develop.we-service.cn/huaxin/tms-api"', // 测试环境
     BASE_NODE_API: '"http://develop.we-service.cn/tms/nodejs/api"', // node测试环境
     BASE_SYS_API: '"http://192.168.1.110:9919"', // 测试系统服务
     IMG_API: '"http://develop.we-service.cn/hdd/image/"' // 测试环境
