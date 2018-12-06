b2cee5032c9c9631882abadcae8fbf9dd87d81af
Caoanlong <caoanlong@gmail.com>
2018-12-06 17:42:46 +0800

    config

--- config/prod.env.js
+++ config/prod.env.js
@@ -2,7 +2,7 @@
 module.exports = {
 	NODE_ENV: '"production"',
 	ENV_CONFIG: '"prod"',
-	BASE_API: '"http://api.hdd56.com/tms"', // 生产环境
+	BASE_API: '"http://hxtms.hdd56.com/api"', // 生产环境
 	BASE_NODE_API: '"http://api.hdd56.com/tms_nodejs"',   // node生产环境
 	BASE_SYS_API: '"http://192.168.1.110:9919"', // 生产系统服务
 	IMG_API: '"http://image.hdd56.com/"'  // 生产环境
